9a87d44e9f7739907847f71f146eda07c5b781f7
Herry JIa <herryjia4@gmail.com>
2023-04-12 09:05:33 -0400

    Update MainGame.java

--- src/textadventure/MainGame.java
+++ src/textadventure/MainGame.java
@@ -4,7 +4,7 @@ public class MainGame {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hi");
+		System.out.println("Hi github");
 		for (int i = 0; i < args.length; i++) {
 			
 		}
