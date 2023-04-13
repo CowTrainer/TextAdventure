a6ea0dd6e43113f67f915f8115bb274e207a352c
shendrew <andrewsh190@gmail.com>
2023-04-13 12:26:20 -0400

    string parser for movement

--- src/textadventure/MainGame.java
+++ src/textadventure/MainGame.java
@@ -1,13 +1,35 @@
 package textadventure;
 
+import java.util.Scanner;
+
 public class MainGame {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hi github");
-		for (int i = 0; i < 10; i++) {
-			System.out.println('*');
-		}
+
+        Scanner input = new Scanner(System.in);
+        String word;
+		
+        while (true) {
+            word = input.nextLine();
+            String[] words = word.split("\\s");
+
+            for (int i=0; i<words.length; i++) {
+                words[i] = words[i].toLowerCase();
+            }
+            
+            switch (words[0]) {
+                case "n": case "north": case "s": case "south": case: "e": case "east": case "w": case "west":
+                    moveToRoom(words[0]);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
 	}
 
+    void moveToRoom() {
+        
+    }
 }
