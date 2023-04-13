b055072af3b6cd92a7b8c5eee86b51ffa186a35e
shendrew <andrewsh190@gmail.com>
2023-04-13 13:12:17 -0400

    game setup, completed room class

--- src/textadventure/MainGame.java
+++ src/textadventure/MainGame.java
@@ -1,26 +1,28 @@
 package textadventure;
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class MainGame {
+    static HashMap<String, Room> roomMap = new HashMap<>();
+    static String curRoom;
+    static boolean isPlaying = true;
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
 
-        Scanner input = new Scanner(System.in);
-        String word;
+        setup();
 		
-        while (true) {
-            word = input.nextLine();
-            String[] words = word.split("\\s");
+        while (isPlaying) {
 
-            for (int i=0; i<words.length; i++) {
-                words[i] = words[i].toLowerCase();
-            }
+            String command = getCommand();
+            String[] words = command.split("\\s");
             
-            switch (words[0]) {
-                case "n": case "north": case "s": case "south": case: "e": case "east": case "w": case "west":
-                    moveToRoom(words[0]);
+            String word1 = words[0];
+            switch (word1) {
+                case "n": case "s": case "e": case "w":
+                case "north": case "south": case "east": case "west":
+                    moveToRoom(word1.charAt(0));
+                    System.out.println(roomMap.get(curRoom).getName());
                     break;
             
                 default:
@@ -29,7 +31,26 @@ public class MainGame {
         }
 	}
 
-    void moveToRoom() {
-        
+    static void setup() {
+        Room.setRooms(roomMap);
+        curRoom = "hallway";
+    }
+
+    static Scanner sc = new Scanner(System.in);
+    static String getCommand() {
+		System.out.print("=> ");
+		String text = sc.nextLine().toLowerCase();
+		if (text.length() == 0) text = "look"; //default command		
+		return text;
+	}
+
+    static void moveToRoom(char direction) {
+        String newRoom = roomMap.get(curRoom).getExit(direction);
+
+        if (newRoom.length() == 0) {
+            System.out.println("You can't go that way.");
+            return;
+        }
+        curRoom = newRoom;
     }
 }
---
+++ src/textadventure/Room.java
@@ -0,0 +1,45 @@
+package textadventure;
+
+import java.util.HashMap;
+
+class Room {
+    private String name;
+    private String description;
+    private String n, s, e, w;
+
+    private Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    private void setExits(String n, String s, String e, String w) {
+        this.n = n;
+        this.s = s;
+        this.e = e;
+        this.w = w;
+    }
+
+    String getExit(char direction) {
+        switch (direction) {
+            case 'n': return n;
+            case 's': return s;
+            case 'e': return e;
+            case 'w': return w;
+        }
+        return null;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getDescription() {
+        return this.description;
+    }
+
+    static void setRooms(HashMap<String, Room> map) {
+        Room room = new Room("Hallway", "This is the hallway");
+        room.setExits("workshop", " office", "entrance", "caf");
+        map.put("hallway", room);
+    }
+}
