9de79909a4548172f5f9cbe80eb99dbb7d3795dc
shendrew <andrewsh190@gmail.com>
2023-04-18 23:03:32 -0400

    set up rooms

--- src/textadventure/MainGame.java
+++ src/textadventure/MainGame.java
@@ -7,9 +7,9 @@ public class MainGame {
     static HashMap<String, Room> roomMap = new HashMap<>();
     static String curRoom;
     static boolean isPlaying = true;
+    static int turnCounter = 0;
 
 	public static void main(String[] args) {
-
         setup();
 		
         while (isPlaying) {
@@ -22,18 +22,24 @@ public class MainGame {
                 case "n": case "s": case "e": case "w":
                 case "north": case "south": case "east": case "west":
                     moveToRoom(word1.charAt(0));
-                    System.out.println(roomMap.get(curRoom).getName());
                     break;
-            
+                case "look":
+                    showRoom();
+                // case "i": case "inventory":
+                    // if ()
                 default:
-                    break;
+                    System.out.println("**Invalid command**");
             }
+
+            checkGameOver();
         }
 	}
 
     static void setup() {
         Room.setRooms(roomMap);
-        curRoom = "hallway";
+        curRoom = "detention";
+        System.out.println("Darn, you were caught 'using the washroom' again, and got sent to the office. It's well after school, but the principal forgot to let you out of the detention room. Escape the school.");
+        showRoom();
     }
 
     static Scanner sc = new Scanner(System.in);
@@ -52,5 +58,27 @@ public class MainGame {
             return;
         }
         curRoom = newRoom;
+        showRoom();
+        turnCounter++;
+    }
+
+    static void showRoom() {
+        Room room = roomMap.get(curRoom);
+        System.out.println("-- "+room.getName()+" --");
+        System.out.println(room.getDescription());
+    }
+
+    static void checkGameOver() {
+        if (curRoom == "fire") {
+            System.out.println("You pushed open the fire exit door and sounded the alarm. You were caught by the custodian and reported to the principal.");
+            System.out.println("MISSION FAILED");
+            System.exit(0);
+        }
+
+        if (turnCounter == 20) {
+            System.out.println("You pulled an all-nighter studying last night, and haven't ate anything since. You fainted.");
+            System.out.println("MISSION FAILED");
+            System.exit(0);
+        }
     }
 }
